fe164e80ab35056485bea437bbaeb335a400603d
Huseyin Enes ERTURK <huseyineneserturk@gmail.com>
2026-02-24 15:33:54 +0300

    chore: initial project setup with Vite + React + TS

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarimi ve Programlama</h1>
+      <h2>LAB-1</h2>
+      <p>Ad Soyad: Hüseyin Enes Ertürk</p>
+      <p>Ogrenci No: 235541019</p>
+    </div>
+  )
+}
+
+export default App
